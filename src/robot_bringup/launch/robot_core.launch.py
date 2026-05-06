e42bebf5036acc09f58b9ff506ecc01e06c55797
Bkekule <borisatud@icloud.com>
2026-05-06 05:11:28 -0500

    refactor: update controller configuration and launch file to use ExecuteProcess

--- src/robot_bringup/launch/robot_core.launch.py
+++ src/robot_bringup/launch/robot_core.launch.py
@@ -2,11 +2,12 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import RegisterEventHandler
+from launch.actions import ExecuteProcess, RegisterEventHandler
 from launch.event_handlers import OnProcessStart
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackagePrefix
 
 
 def generate_launch_description() -> LaunchDescription:
@@ -52,24 +53,26 @@ def generate_launch_description() -> LaunchDescription:
         executable='ros2_control_node',
         parameters=[
             {
-                'robot_description': robot_description_content,
-                'use_sim_time': LaunchConfiguration('use_sim_time'),
-            },
-            controllers_yaml,
+                'update_rate': 100,
+                'diff_drive_controller': {'type': 'diff_drive_controller/DiffDriveController'},
+                'joint_state_broadcaster': {'type': 'joint_state_broadcaster/JointStateBroadcaster'},
+            }
         ],
         output='screen',
     )
 
-    joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
+    spawner_bin = PathJoinSubstitution(
+        [FindPackagePrefix('controller_manager'), 'lib', 'controller_manager', 'spawner']
     )
 
-    diff_drive_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['diff_drive_controller'],
+    joint_state_broadcaster_spawner = ExecuteProcess(
+        cmd=[spawner_bin, 'joint_state_broadcaster'],
+        output='screen',
+    )
+
+    diff_drive_controller_spawner = ExecuteProcess(
+        cmd=[spawner_bin, 'diff_drive_controller', '--param-file', controllers_yaml],
+        output='screen',
     )
 
     spawn_controllers = RegisterEventHandler(
@@ -81,6 +84,7 @@ def generate_launch_description() -> LaunchDescription:
 
     return LaunchDescription(
         [
+            SetParameter('use_sim_time', LaunchConfiguration('use_sim_time')),
             robot_state_publisher_node,
             controller_manager_node,
             spawn_controllers,
